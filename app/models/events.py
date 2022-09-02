3ea9bfb5110f08884b2cc96f617246a36d35f0e4
Attiya <adarensburg@live.com>
2022-09-02 16:16:45 -0500

    recovering work from this morning, moving on to creating post method in event store

--- app/models/events.py
+++ app/models/events.py
@@ -5,8 +5,8 @@ class Events(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     host_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
     event_name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(2000), nullable=False)
     event_image = db.Column(db.String, nullable=False)
