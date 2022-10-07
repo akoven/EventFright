2b4e209eae8d92d3fa3de5ab0a651230adffbf0e
Attiya <adarensburg@live.com>
2022-10-07 13:00:27 -0500

    still working on pg formatting for tickets/registration

--- react-app/src/components/EventRegistration/index.js
+++ react-app/src/components/EventRegistration/index.js
@@ -24,12 +24,19 @@ const EventRegistration = () =>{
         <div className='registration-pg'>
             <div className='registration-div'>
                 <header className='registration-header'><NavLink to={'/'} className='registration-homepage-link'>Event Fright</NavLink></header>
-                <img className='selected-img' src={selectedEvent[0].event_image}/>
-                <h2>{selectedEvent[0].date}</h2>
-                <h1 className='selected-event-title'>{selectedEvent[0].event_name}</h1>
-                <h3>About this event:</h3>
-                <p className='selected-event-description'>{selectedEvent[0].description}</p>
-            <h2>Tickets</h2>
+                <div className='event-info-ticket-div'>
+                    <div className='event-info-div'>
+                        <img className='selected-img' src={selectedEvent[0].event_image}/>
+                        <h2>{selectedEvent[0].date}</h2>
+                        <h1 className='selected-event-title'>{selectedEvent[0].event_name}</h1>
+                        <h3>About this event:</h3>
+                        <p className='selected-event-description'>{selectedEvent[0].description}</p>
+                    </div>
+                    <div className='registration-tickets-span'>
+                        <h4>price shows here</h4>
+                        <button className='tickets-btn-on-reg-pg'>Tickets</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
