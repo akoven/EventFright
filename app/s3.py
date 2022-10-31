d7e7ad14b660e29ac270bfa99a89a14444e1bb70
Attiya <adarensburg@live.com>
2022-10-31 10:00:00 -0500

    aws commit

---
+++ app/s3.py
@@ -0,0 +1,34 @@
+import boto3
+# import os
+import uuid
+
+
+BUCKET_NAME="event-fright-aws-bucket"
+allowed_extension_file_types = {'jpg','jpeg','png'}
+S3_location = f'https://{BUCKET_NAME}.s3.amazonaws.com/'
+
+s3=boto3.client("s3",
+    aws_access_key_id = 'AKIAZMZCNF27MGM6VMM7',
+    aws_secret_access_key = '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'
+)
+
+def extensions(filename):
+    return '.' in filename and \
+        filename.rsplit(".", 1)[1].lower() in allowed_extension_file_types
+
+def get_unique_filename(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
+
+def upload_to_s3(file):
+    try:
+        s3.upload_fileobj(
+            file, BUCKET_NAME,file.filename,ExtraArgs = {
+                "ACL":"public-read",
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        return {"errors":str(e)}
+    return {"url":f"{S3_location}{file.filename}"}
